5ec1f71babd08f11c3b2eec8a9cea87cc50fde54
eqtstv <38236287+eqtstv@users.noreply.github.com>
2019-09-25 00:10:19 +0200

    Updates

---
+++ precache-manifest.4e6aee6321efa510ef34052e22588384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665a1cd351e9f9ebddd23e86d528383d",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "f6c6d430791a11d5b47c",
+    "url": "/Weather-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7a3650f1b05ad7109e43",
+    "url": "/Weather-app/static/css/main.91ffdc8a.chunk.css"
+  },
+  {
+    "revision": "f6c6d430791a11d5b47c",
+    "url": "/Weather-app/static/js/2.eef67a0e.chunk.js"
+  },
+  {
+    "revision": "7a3650f1b05ad7109e43",
+    "url": "/Weather-app/static/js/main.ced3bb53.chunk.js"
+  },
+  {
+    "revision": "72270baf87acbae1722c",
+    "url": "/Weather-app/static/js/runtime-main.560bda6b.js"
+  },
+  {
+    "revision": "c759028d65b01f3d32b0e7cdd6c37e28",
+    "url": "/Weather-app/static/media/bg.c759028d.jpg"
+  }
+]);
